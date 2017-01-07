d6e90ac9f5f0da528d7cb533186535bcf6b3494e
Ludwig, Sven <Sven.Ludwig@senacor.com>
2017-01-07 23:50:48 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Code Camp Akka Streams using Java DSL
+
+Code Camp for using Akka Streams via its Java DSL for stream processing.
+
+Kafka is used to persist streams.
+
---
+++ src/main/java/net/codecamp/akka/streams/java/kata01/Kata01Source.java
@@ -0,0 +1,21 @@
+package net.codecamp.akka.streams.java.kata01;
+
+import akka.NotUsed;
+import akka.stream.javadsl.Source;
+
+class Kata01Source {
+
+    /**
+     * Task: Create a akka.stream.javadsl.Source that emits the first ten natural numbers before completing.
+     * <p/>
+     * Tip: use the appropriate static factory method to be found in akka.stream.javadsl.Source to create the source.
+     * <p/>
+     * Reference: http://doc.akka.io/docs/akka/2.4.16/java/stream/stream-flows-and-basics.html
+     *
+     * @return The source.
+     */
+    static Source<Integer, NotUsed> createSourceOfNaturalNumbers1to10() {
+        return null; // TODO solve
+    }
+
+}
