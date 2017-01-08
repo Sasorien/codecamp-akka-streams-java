624ddf33fe0dcfe9107d2f9c9aaa9e0a37f43c7f
Ludwig, Sven <Sven.Ludwig@senacor.com>
2017-01-08 01:29:52 +0100

    kata02

---
+++ src/main/java/net/codecamp/akka/streams/java/kata02/Kata02FileSource.java
@@ -0,0 +1,23 @@
+package net.codecamp.akka.streams.java.kata02;
+
+import akka.stream.IOResult;
+import akka.stream.javadsl.Source;
+
+import java.util.concurrent.CompletionStage;
+
+class Kata02FileSource {
+
+    /**
+     * Task: Create a akka.stream.javadsl.Source that emits line by line of kata02.txt file.
+     * <p/>
+     * Reference: http://doc.akka.io/docs/akka/2.4.16/java/stream/stream-io.html#Streaming_File_IO
+     * <p/>
+     * Check: The kata is solved when the corresponding unit test is green.
+     *
+     * @return The source.
+     */
+    static Source<String, CompletionStage<IOResult>> createSourceOfLinesInFileKata02Txt() {
+        return null;
+    }
+
+}
